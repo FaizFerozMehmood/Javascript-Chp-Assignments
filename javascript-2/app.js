3db2b3b7d2abf697525e34b27ec875a25fdb7f21
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-03-01 02:02:25 +0500

    Javascript-Assignments

---
+++ javascript-2/app.js
@@ -0,0 +1,83 @@
+//1 to 10
+/*
+for( var i = 1; i <= 10 ; i ++){
+  console.log (i)
+}
+
+*/
+//2
+//console.log("Sum:", sum);*/
+/*
+var sum = 0;
+for (var i = 1;i <=10 ; i++){
+ sum += i;
+  console.log(i) */
+
+/*
+
+let sum = 0;
+
+for (let i = 1; i <= 10; i++) {
+  sum += i;
+  //console.log(i)
+console.log("Sum:", sum);
+}
+/*
+//Reverse counting with For Loop
+for (var i = 10; i>= 1;i-- ){
+  console.log (i)
+}
+
+*/
+
+
+/*
+
+for(var i = 1; i<=5;i++){
+  console.log(i)
+}
+
+*/
+
+var sum =0;
+for (var i = 1; i <=10; i++){
+  sum +=i;
+}
+  console.log ( "Answer ", sum)
+
+/*
+let sum = 0;
+
+for (let i = 1; i <= 10; i++) {
+  sum += i;
+}
+
+console.log("The sum of numbers from 1 to 10 is:", sum);
+*/
+//MultiplicationTable
+/*
+for( var i= 1 ; i <11; i++ ){
+  document.write(2+" × "+i+" = " +2*i)
+  
+}
+var input=+ prompt(" Enter any number");
+for ( var i = 1; i <10; i++ ){
+  console.log ( input+ " ×"+ "="+ input*i)
+}
+*/
+/*
+for (var i = 1; i<=5 ; i++){
+  var n ="";
+  for(var k= 1; k<=1; k++){
+    n+="*";
+  }
+  console.log(n)
+}
+*/
+
+// importance of "For loop"in JavaScript :
+
+// For loop can easily manipulate values 
+// Apply if else condition easily
+// Changing values of an array 
+// THese are some major importance of "For loop".
